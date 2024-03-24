9074dd6e110d3e1316d1970d8a5bfcff6729f4da
Hyerin Sung <hyerinx@naver.com>
2024-03-24 21:51:08 +0900

    fix: Add export option to useClick hook

--- useClick.js
+++ useClick.js
@@ -1,17 +1,17 @@
-const useClick = (onClick) => {
-    if (typeof onClick !== "function") {
-      return;
+export const useClick = (onClick) => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
     }
-    const element = useRef();
-    useEffect(() => {
+    return () => {
       if (element.current) {
-        element.current.addEventListener("click", onClick);
+        element.current.removeEventListener("click", onClick);
       }
-      return () => {
-        if (element.current) {
-          element.current.removeEventListener("click", onClick);
-        }
-      };
-    }, []);
-    return element;
-  };
+    };
+  }, []);
+  return element;
+};
